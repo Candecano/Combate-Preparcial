ca3bb2876377e28dba16e421d702c27ec0b9b29e
Carla Marturet <abrevadero.4020@gmial.com>
2024-09-11 16:34:26 -0300

    CREACCION DELA CLASECOMBATE

---
+++ demo/src/main/java/com/example/Combate.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Combate {
+    private int combate; 
+
+    public int getCombate() {
+        return combate;
+    }
+
+    public void setCombate(int combate) {
+        this.combate = combate;
+    }
+
+
+    
+}
